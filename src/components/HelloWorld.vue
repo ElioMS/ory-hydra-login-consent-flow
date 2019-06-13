9b35e12e0328d8f993e19ec82e5ec4630825c0a8
Elio G. Solis <elio.garcia.solis@gmail.com>
2019-06-13 15:05:24 -0500

    deleting comments

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,6 @@
             <h2> Redirect to Login page</h2>
             <a href="http://192.168.1.113:4444/oauth2/auth?audience=&client_id=azteca1&max_age=0&nonce=telpxsykzqbkeihofjdkbfyj&response_type=code&state=npsblubblzoxdsrwgtxvhdhe&redirect_uri=http%3A%2F%2F192.168.1.113%3A8081%2Fcallback"> Redirect </a>
 
-<!--        http://localhost:4444/oauth2/auth?audience=&client_id=spring-client&max_age=0-->
-<!--        &nonce=telpxsykzqbkeihofjdkbfyj&prompt=&redirect_uri=http%3A%2F%2F127.0.0.1%3A4446%2Fcallback-->
-<!--        &response_type=code&scope=openid+offline&state=npsblubblzoxdsrwgtxvhdhe-->
     </v-container>
 </template>
 
